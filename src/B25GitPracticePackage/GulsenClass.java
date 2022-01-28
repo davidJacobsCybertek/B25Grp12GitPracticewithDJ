1ccbfc377c992334eaae1f90e68adfd91b37c62e
Gulsen_Sohret <93159111+Gulsen927@users.noreply.github.com>
2022-01-28 09:26:41 -0500

    GulsenClass.java step 2 code

--- src/B25GitPracticePackage/GulsenClass.java
+++ src/B25GitPracticePackage/GulsenClass.java
@@ -3,5 +3,6 @@ package B25GitPracticePackage;
 public class GulsenClass {
     public static void main(String[] args) {
         System.out.println("Hello Group12");
+        System.out.println("Our new group is 11");
     }
 }
