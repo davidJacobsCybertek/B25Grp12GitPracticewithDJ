c59ad4d7f731e5954f48d36e31b33ed5849ecc37
brandoncabrera <brandonfcab@gmail.com>
2022-01-29 18:48:05 -0500

    Brandon's test case 001 started

--- src/B25GitPracticePackage/BrandonsTC001.java
+++ src/B25GitPracticePackage/BrandonsTC001.java
@@ -1,8 +1,14 @@
 package B25GitPracticePackage;
 
-public class David {
+public class BrandonsTC001 {
     public static void main(String[] args) {
-        System.out.println("smth");
+
         System.out.println("testing. This is Brandon");
+
+        System.out.println("Hello Group 11/Group 12");
+
+        // testing testing....coding...coding
+
+
     }
 }
