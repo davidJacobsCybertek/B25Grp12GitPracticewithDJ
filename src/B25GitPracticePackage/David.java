ea788bad28c715a4ec44626fa5e9a146ee411a3c
Jewel522 <hayatliq@hotmail.com>
2022-02-01 09:36:13 -0500

    commit 1 Jewel

--- src/B25GitPracticePackage/David.java
+++ src/B25GitPracticePackage/David.java
@@ -3,5 +3,10 @@ package B25GitPracticePackage;
 public class David {
     public static void main(String[] args) {
         System.out.println("smth");
+        int num2 = 5;
+        //Hello From Jewel 20220201 create merge conflict
     }
+
+
+
 }
